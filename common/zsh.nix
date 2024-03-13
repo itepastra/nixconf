d6c71d2705f773a3d3e1267b79ccbc712a13148b
Noa Aarts <itepastra@gmail.com>
2024-03-13 18:05:37 +0100

    add direnv, remove python

--- common/zsh.nix
+++ common/zsh.nix
@@ -4,6 +4,11 @@
 	home.packages = with pkgs; [
 		zsh
 	];
+	programs.direnv = {
+		enable=true;
+		enableZshIntegration=true;
+		nix-direnv.enable=true;
+	};
 	programs.zsh = {
 		enable=true;
 		shellAliases = {
