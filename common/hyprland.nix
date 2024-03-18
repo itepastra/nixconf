3ced93d1d254cf243e1287dfc2ff4439e02510bf
Noa Aarts <itepastra@gmail.com>
2024-03-18 12:10:00 +0100

    update screen layout and nixpkgs

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -21,8 +21,8 @@
 		enable = true;
 		settings = {
 			monitor = [
-				"DP-3,2560x1440@144,1920x0,1"
-				"DP-2,1920x1080@60,0x0,1"
+				"DP-3,2560x1440@360,1920x0,1,bitdepth,10,vrr,1"
+				"DP-2,2560x1440@144,0x0,1,bitdepth,10,vrr,1"
 			];
 			windowrulev2 = [
 				"opacity 1.0 0.6,class:^(kitty)$"
