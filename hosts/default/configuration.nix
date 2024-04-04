9890562eef5534cfdb036664d36fbf7601238f65
Noa Aarts <itepastra@gmail.com>
2024-04-04 14:48:10 +0200

    disable auto optimise store

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -35,7 +35,7 @@
 	nix = {
 		settings = {
 			# auto optimise every so often
-			auto-optimise-store = true;
+			# auto-optimise-store = true;
 			experimental-features = ["nix-command" "flakes"];
 			substituters = ["https://hyprland.cachix.org" "https://cache.iog.io"];
 			trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
