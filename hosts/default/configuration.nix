9d0703827954b5cb382ec13babf81e7125ddb61f
Noa Aarts <itepastra@gmail.com>
2024-04-29 23:56:59 +0200

    hardcode driver file path

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -38,6 +38,10 @@ rec {
 		package = config.hardware.nvidia.package;
 	};
 
+	environment.variables = {
+		VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json;
+	};
+
 	# Allow unfree packages
 	nixpkgs.config = {
 		allowUnfree = true;
