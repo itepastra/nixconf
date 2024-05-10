2b2218a2c7919bc0204c7f32530e79d4ce267171
Noa Aarts <itepastra@gmail.com>
2024-05-11 01:08:08 +0200

    re-enable zsh aliases

--- modules/applications/default.nix
+++ modules/applications/default.nix
@@ -19,7 +19,10 @@ in
 			firefox.enable = true;
 			git.enable = true;
 			kitty.enable = true;
-			zsh.enable = true;
+			zsh = {
+				enable = true;
+				enableAliases = true;
+			};
 		};
 	};
 }
