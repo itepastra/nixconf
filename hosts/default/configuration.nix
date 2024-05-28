b4259d9caae3f6d7bce3a885fe26a294fdcba0e8
Noa Aarts <itepastra@gmail.com>
2024-05-28 21:43:03 +0200

    add nginx proxy for sods

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -227,6 +227,31 @@
 					forceSSL = false;
 					addSSL = true;
 					locations = {
+						"/sods/" = {
+							extraConfig = ''
+# allow large file uploads
+								client_max_body_size 50000M;
+
+# Set headers
+							proxy_set_header Host              $host;
+							proxy_set_header X-Real-IP         $remote_addr;
+							proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
+							proxy_set_header X-Forwarded-Proto $scheme;
+							proxy_set_header X-Forwarded-Prefix sods;
+
+# enable websockets: http://nginx.org/en/docs/http/websocket.html
+							proxy_http_version 1.1;
+							proxy_set_header   Upgrade    $http_upgrade;
+							proxy_set_header   Connection "upgrade";
+							proxy_redirect     off;
+
+# set timeout
+							proxy_read_timeout 600s;
+							proxy_send_timeout 600s;
+							send_timeout       600s;
+							'';
+							proxyPass = "http://127.0.0.1:2000/";
+						};
 						"/" = {
 							extraConfig = ''
 # allow large file uploads
