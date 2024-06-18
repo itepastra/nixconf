9f25e72fedd617f3e5faf2f72bef6ade205f506f
Noa Aarts <itepastra@gmail.com>
2024-06-18 23:47:21 +0200

    remove comment

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -69,13 +69,7 @@
   networking = {
     hostName = "lambdaOS"; # Define your hostname.
   };
-  # networking.wireless.enable = true;# Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
   programs.nm-applet.enable = true;
