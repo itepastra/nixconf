044d34b0300a296812c04aeff93976b3b78b86d0
Noa Aarts <itepastra@gmail.com>
2024-06-19 11:55:32 +0200

    aaaaa

--- modules/plasma/default.nix
+++ modules/plasma/default.nix
@@ -7,13 +7,8 @@ in
     enable = lib.mkEnableOption "enable kde plasma 6";
   };
 
-  imports = [
-		../applications
-  ];
-
 
   config = lib.mkIf cfg.enable {
-		modules.apps.enable = true;
 		services.desktopManager.plasma6.enable = true;
 
 		xdg.portal.config.common.default = "*";
