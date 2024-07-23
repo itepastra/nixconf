a874e9702723fe97e17156e51d46e87572c417e6
Noa Aarts <itepastra@gmail.com>
2024-07-23 22:09:13 +0200

    chore: remove discord, keep vesktop only

--- common/discord/discord.nix
+++ common/discord/discord.nix
@@ -2,14 +2,6 @@
 
 {
   home.packages = with pkgs; [
-     (symlinkJoin {
-     	name = "discord";
-     	paths = [
-     		(writeShellScriptBin "discord" ''${discord}/bin/discord --enable-features=UseOzonePlatform --ozone-platform=wayland'')
-     		(writeShellScriptBin "Discord" ''${discord}/bin/Discord --enable-features=UseOzonePlatform --ozone-platform=wayland'')
-     		discord
-     	];
-     })
     (symlinkJoin {
       name = "vesktop";
       paths = [
