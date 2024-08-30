18456e3c0d403bdcbc4bf2ebe96fef61348e10b8
Noa Aarts <noa@voorwaarts.nl>
2024-08-30 20:24:08 +0200

    fix: allow uris for hydra

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -34,6 +34,11 @@
       experimental-features = [ "nix-command" "flakes" ];
       substituters = [ "https://hyprland.cachix.org" ];
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+
+      allowed-uris = [
+        "github:"
+        "gitlab:"
+      ];
     };
     gc.automatic = false;
   };
