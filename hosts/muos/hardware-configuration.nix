dd2b4f9023fa9f96d5142c2ff247ada5dec65d4f
Noa Aarts <noa@voorwaarts.nl>
2024-08-30 19:28:17 +0200

    fix: make hardware-configuration identical

--- hosts/muos/hardware-configuration.nix
+++ hosts/muos/hardware-configuration.nix
@@ -9,7 +9,7 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "thunderbolt" "xhci_pci" "ahci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -21,8 +21,6 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp42s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = {
-    system = "x86_64-linux";
-  };
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
