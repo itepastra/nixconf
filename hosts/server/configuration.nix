5865c7cb0240224c76e3327da24d414bcb1a3470
Noa Aarts <noa@voorwaarts.nl>
2024-08-31 11:55:56 +0200

    fix: disable nextcloud again

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -287,12 +287,6 @@
       relayHost = "mail.voorwaarts.nl";
       relayPort = 587;
     };
-    nextcloud = {
-      enable = true;
-      package = pkgs.nextcloud29;
-      hostName = "nc.itepastra.nl";
-      config.adminpassFile = "/etc/nextcloud_admin_pass";
-    };
   };
 
   mailserver = {
