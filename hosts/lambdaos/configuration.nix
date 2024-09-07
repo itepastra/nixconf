378f6892a520fd54bd1345b4e3e65ab5ed6a636e
Noa Aarts <noa@voorwaarts.nl>
2024-09-07 15:52:29 +0200

    feat: remove unused restic user

--- hosts/lambdaos/configuration.nix
+++ hosts/lambdaos/configuration.nix
@@ -123,17 +123,7 @@
   environment.systemPackages = with pkgs; [
     restic
   ];
-  users.users.restic = {
-    isNormalUser = true;
-  };
 
-  security.wrappers.restic = {
-    source = "${pkgs.restic.out}/bin/restic";
-    owner = "restic";
-    group = "users";
-    permissions = "u=rwx,g=,o=";
-    capabilities = "cap_dac_read_search=+ep";
-  };
 
   # TODO: find list of fonts to install
   fonts.packages = with pkgs; [
