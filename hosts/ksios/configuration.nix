5f8f2c1453512e27bf2dec8f948eb7b6010fb4b8
Noa Aarts <noa@voorwaarts.nl>
2024-09-14 21:02:19 +0200

    feat: remove more stuffs

--- hosts/ksios/configuration.nix
+++ hosts/ksios/configuration.nix
@@ -24,8 +24,6 @@
     hostName = "ksios"; # Define your hostname.
     networkmanager.enable = true; # Easiest to use and most distros use this by default.
   };
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;	# Enables wireless support via wpa_supplicant.
 
   nix = {
     settings = {
