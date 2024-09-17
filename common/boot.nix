2a66fcb6148b61c259e6661176f36f8fd9de4e57
Noa Aarts <noa@voorwaarts.nl>
2024-09-17 11:04:48 +0200

    fix: format boot

--- common/boot.nix
+++ common/boot.nix
@@ -1,12 +1,12 @@
-{lib,...}:
+{ lib, ... }:
 {
-	boot.loader = {
-		timeout = lib.mkDefault 0;
-		efi.canTouchEfiVariables = true;
-		systemd-boot = {
-			enable = true;
-			editor = false;
-			configurationLimit = 100;
-		};
-	};
+  boot.loader = {
+    timeout = lib.mkDefault 0;
+    efi.canTouchEfiVariables = true;
+    systemd-boot = {
+      enable = true;
+      editor = false;
+      configurationLimit = 100;
+    };
+  };
 }
