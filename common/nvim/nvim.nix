f6a3111d4a89b7b07b643fb08957007c34fcee20
Noa Aarts <noa@voorwaarts.nl>
2024-10-03 21:49:02 +0200

    feat: nicer neovim thingies

--- common/nvim/nvim.nix
+++ common/nvim/nvim.nix
@@ -8,35 +8,38 @@
     programs.neovim =
       {
         enable = true;
-        extraPackages = with pkgs; [
-          ripgrep
-          luarocks
-          gnumake
-          wget
-          nixpkgs-fmt
-          tree-sitter
-          fd
-
-          (lib.mkIf config.modules.apps.neovim.enableLanguages cargo)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages gcc)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages go)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages jdk22)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages lua51Packages.lua)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages nodejs)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages php83Packages.composer)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages php83)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages opam)
-          (lib.mkIf config.modules.apps.neovim.enableLanguages
+        extraPackages = with pkgs; lib.mkMerge [
+          [
+            ripgrep
+            luarocks
+            gnumake
+            wget
+            nixpkgs-fmt
+            tree-sitter
+            fd
+          ]
+          (lib.mkIf config.modules.apps.neovim.enableLanguages [
+            cargo
+            gcc
+            go
+            jdk22
+            luaPackages.lua
+            nodejs
+            php83Packages.composer
+            php83
+            opam
             (python3.withPackages (python-pkgs: [
               python-pkgs.pip
               python-pkgs.black
-            ])))
+            ]))
+          ])
         ];
         defaultEditor = true;
         viAlias = true;
         vimAlias = true;
 
-        extraLuaConfig = lib.fileContents ./init.lua;
+        extraLuaConfig = lib.fileContents
+          ./init.lua;
       };
   };
 }
