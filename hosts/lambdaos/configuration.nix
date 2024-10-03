ee7f436e6257b91ea8482570bf54fa4eb8b52670
Noa Aarts <noa@voorwaarts.nl>
2024-10-03 16:53:57 +0200

    feat: add iohk to binary substitutors

--- hosts/lambdaos/configuration.nix
+++ hosts/lambdaos/configuration.nix
@@ -67,12 +67,14 @@
         "https://cache.iog.io"
         "https://cuda-maintainers.cachix.org"
         "https://nix-community.cachix.org/"
+        "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
       allowed-uris = [
         "github:"
