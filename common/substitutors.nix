d7c32eafca2c29f67fd5c4017cf9fed1ac6e7e97
Noa Aarts <noa@voorwaarts.nl>
2024-10-21 15:19:10 +0200

    feat: add system-features, remove allowed-uris

--- common/substitutors.nix
+++ common/substitutors.nix
@@ -2,7 +2,7 @@
   nix = {
     settings = {
       # auto optimise every so often
-      # auto-optimise-store = true;
+      auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
       substituters = [
         (lib.mkIf (config.networking.hostName != "nuOS") "https://cache.itepastra.nl")
@@ -20,12 +20,12 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
+      system-features = [ "kvm" "big-parallel" ];
       allowed-uris = [
         "github:"
         "gitlab:"
         "https://github.com/"
       ];
     };
-    optimise.automatic = true;
   };
 }
