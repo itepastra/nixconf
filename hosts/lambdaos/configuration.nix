d5388679c927fc224f70e2e435a6d070c818af39
Noa Aarts <noa@voorwaarts.nl>
2025-03-25 02:29:46 +0100

    nvidia cuda shit

--- hosts/lambdaos/configuration.nix
+++ hosts/lambdaos/configuration.nix
@@ -172,8 +172,26 @@
     };
   };
 
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    pciutils
+    file
+
+    gnumake
+    gcc
+
+    cudatoolkit
+  ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  systemd.services.nvidia-control-devices = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";
+  };
+
+  nixpkgs.config.allowUnfree = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
