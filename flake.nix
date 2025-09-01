c6dad32c47656686d78b1a74a100d05f0349417e
Noa Aarts <noa@voorwaarts.nl>
2025-09-01 22:33:33 +0200

    nixos-unstable maybe?

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/eb60a3b";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # nix based ssg
     nixsg.url = "github:itepastra/nixsg/no-md-parser";
