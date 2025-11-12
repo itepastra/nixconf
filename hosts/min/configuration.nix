5f406a65301a54111952972f458cfe1510adf6d6
Noa Aarts <noa@voorwaarts.nl>
2025-11-12 09:23:06 +0100

    add min host

---
+++ hosts/min/configuration.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  imports = [
+    ../nuos/disk-config.nix
+  ];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  users.users = {
+    noa = {
+      isNormalUser = true;
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "docker"
+        "libvirt"
+      ];
+      hashedPassword = "$6$rounds=512400$g/s4dcRttXi4ux6c$Z6pKnhJXcWxv0TBSMtvJu5.piETdUBSgBVN7oDPKiQV.lbTYz1r.0XQLwMYxzcvaaX0DL6Iw/SEUTiC2M50wC/";
+      openssh.authorizedKeys.keys = import ../../common/ssh-keys.nix;
+    };
+  };
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
+}
