78f9bf9871edd17d069bd28380a0e1c1d0a203e9
Noa Aarts <noa@voorwaarts.nl>
2025-11-19 09:22:18 +0100

    add pdf mimetype

--- common/home/default.nix
+++ common/home/default.nix
@@ -150,6 +150,7 @@ in
         "x-scheme-handler/https" = "firefox.desktop";
         "x-scheme-handler/about" = "firefox.desktop";
         "x-scheme-handler/unknown" = "firefox.desktop";
+        "application/pdf" = "firefox.desktop";
       };
     };
     # If I have a monitor I want niri with my config, but niri wants it at that spot
