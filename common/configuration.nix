0be23a4b1fd28486b1df1b5258aeaf02f0312eae
Noa Aarts <noa@voorwaarts.nl>
2026-04-20 10:48:27 +0200

    add v4l2loopback when graphics

--- common/configuration.nix
+++ common/configuration.nix
@@ -1,6 +1,7 @@
 { enableGraphics }:
 {
   pkgs,
+  config,
   lib,
   inputs,
   ...
@@ -145,6 +146,12 @@ in
       theme = "colorful";
       themePackages = [ (pkgs.adi1090x-plymouth-themes.override { selected_themes = [ theme ]; }) ];
     };
+
+    extraModulePackages = lib.mkIf enableGraphics [ config.boot.kernelPackages.v4l2loopback.out ];
+    kernelModules = lib.mkIf enableGraphics [ "v4l2loopback" ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    '';
 
     kernelParams = lib.mkIf enableGraphics [
       "plymouth.use-simpledrm"
