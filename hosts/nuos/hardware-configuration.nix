de9fa5b4eb36a5d9c6dd6977674292e6c17c6653
Noa Aarts <noa@voorwaarts.nl>
2026-04-23 13:11:53 +0200

    add to fstab

--- hosts/nuos/hardware-configuration.nix
+++ hosts/nuos/hardware-configuration.nix
@@ -32,4 +32,12 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  fileSystems."/data" = {
+    device = "/dev/sda";
+    fsType = "btrfs";
+    options = [
+      "compress=zstd"
+    ];
+  };
 }
