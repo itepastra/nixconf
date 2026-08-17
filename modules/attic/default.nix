5fe310ba65fd1c51b5fa14778a82b51e04c14575
Noa Aarts <noa@voorwaarts.nl>
2026-08-17 21:27:12 +0200

    fixup! add attic to nuos

--- modules/attic/default.nix
+++ modules/attic/default.nix
@@ -13,12 +13,12 @@
       environmentFile = "/data/attic/private";
       settings = {
         storage.path = "/data/attic/storage";
-        database.url = lib.mkIf config.services.postgresql.enable "postgresql://localhost/attic";
+        database.url = lib.mkIf config.services.postgresql.enable "postgresql://localhost/${config.services.atticd.user}";
       };
     };
 
     postgresql = {
-      ensureDatabases = [ "atticd" ];
+      ensureDatabases = [ "${config.services.atticd.user}" ];
       ensureUsers = [
         {
           name = config.services.atticd.user;
