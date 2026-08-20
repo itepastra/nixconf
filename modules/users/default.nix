b4943ee0172f62f6bb20b4dc05c0a39793f60b44
Noa Aarts <noa@voorwaarts.nl>
2026-08-20 16:44:38 +0200

    remove tracing

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -9,33 +9,31 @@
 
   users = lib.mkMerge [
     ({
-      users = lib.traceValSeq (
-        lib.mapAttrs (
-          user:
-          {
-            name,
-            ssh-keys,
-            password,
-            ...
-          }:
-          {
-            description = lib.mkIf (name != "") name;
-            openssh.authorizedKeys.keys = ssh-keys;
-            isNormalUser = lib.mkIf (user != "root") true;
-            extraGroups = [
-              "networkmanager"
-              "docker"
-              "wireshark"
-              "dialout"
-            ];
-            hashedPassword = password;
-          }
-        ) config.modules.info
-      );
+      users = lib.mapAttrs (
+        user:
+        {
+          name,
+          ssh-keys,
+          password,
+          ...
+        }:
+        {
+          description = lib.mkIf (name != "") name;
+          openssh.authorizedKeys.keys = ssh-keys;
+          isNormalUser = lib.mkIf (user != "root") true;
+          extraGroups = [
+            "networkmanager"
+            "docker"
+            "wireshark"
+            "dialout"
+          ];
+          hashedPassword = password;
+        }
+      ) config.modules.info;
     })
     {
       defaultUserShell = pkgs.zsh;
-      users = lib.traceValSeq {
+      users = {
         noa = lib.mkIf (config.modules.info ? noa) {
           extraGroups = [
             "wheel"
