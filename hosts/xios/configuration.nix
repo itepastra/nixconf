56aa5e97420cb4f094550aa092a29338a1840a4d
Noa Aarts <noa@voorwaarts.nl>
2026-09-02 17:10:31 +0200

    setup for nixosAnywhere

--- hosts/xios/configuration.nix
+++ hosts/xios/configuration.nix
@@ -34,5 +34,13 @@
     })
   ];
 
+  users.users.root.openssh.authorizedKeys.keys = import ../../config/info/ssh-keys.nix;
+
   nixpkgs.hostPlatform = "x86_64-linux";
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
 }
