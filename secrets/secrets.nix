9ab3b03f2daf56dc3266ac8c9b59b651b1bddab3
Noa Aarts <noa@voorwaarts.nl>
2026-09-02 20:41:41 +0200

    fix yubikey and re-make wireguard secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  noa = "AGE-PLUGIN-YUBIKEY-1Q93MKQVZSJ733PC0VWRVC";
+  noa = "age1yubikey1qv9qr2l4srzpt9h9ess58hv4m58njxw50356vgjm92mswul2xnt22fk4t9v";
   nuOS = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDV2IDF4FP1T6qWcv+drtnbKyXkeB9kOAbU4wVWNfPSO";
   muOS = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHw47VIyrawkRLzYKgbd0P6DLDyVvwHfVb5DBPPQUG+d";
   lambdaOS = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICII469nfANk6y6D8gd06OkmxBClpZsNXMW1kxDOreLX";
@@ -43,5 +43,13 @@ in
   "wg/muos.age".publicKeys = [
     noa
     muOS
+  ];
+  "wg/nuos.age".publicKeys = [
+    noa
+    nuOS
+  ];
+  "wg/lambdaos.age".publicKeys = [
+    noa
+    lambdaOS
   ];
 }
