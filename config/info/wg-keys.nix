a9f38957457da9101eeba1e1d52ab1e75fd6653c
Noa Aarts <noa@voorwaarts.nl>
2026-09-03 11:05:32 +0200

    add wireguard public keys to config

---
+++ config/info/wg-keys.nix
@@ -0,0 +1,7 @@
+{
+  keys = {
+    lambdaOS = "noalAv3R9xsUPga9VFvyDfP8F8IH6Hi8vbrGk7YoOAU=";
+    muOS = "nOaMUdcYJ2KCRDcT8GIg0EckoygPCjpt+1MhJD1Vqis=";
+    nuOS = "NOAnUBCzgO1dhioeKp4M7xN2kz+41jKBylhzwxCnwms=";
+  };
+}
