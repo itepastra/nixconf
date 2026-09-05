01e7b1529d35e2fd5629702e9661af7ae94370e7
Noa Aarts <noa@voorwaarts.nl>
2026-09-05 16:38:39 +0200

    remove duplicate proxy_http_version

--- modules/netbird/default.nix
+++ modules/netbird/default.nix
@@ -52,36 +52,52 @@
     };
   };
 
-  services.nginx.virtualHosts."reef.geenit.nl" = {
-    locations."/relay/" = {
-      proxyWebsockets = true;
-      proxyPass = "http://127.0.0.1:29919";
-    };
-    locations."/ws-proxy/" = {
-      proxyWebsockets = true;
-      proxyPass = "http://127.0.0.1:29919";
-    };
-    locations."/signalexchange.SignalExchange/" = {
-      proxyWebsockets = true;
-      proxyPass = "http://127.0.0.1:29919";
-    };
-    locations."/management.ManagementService/" = {
-      proxyWebsockets = true;
-      proxyPass = "http://127.0.0.1:29919";
-    };
-    locations."/api/" = {
-      proxyWebsockets = true;
-      proxyPass = "http://127.0.0.1:29919";
-    };
-    locations."/oauth2/" = {
-      proxyWebsockets = true;
-      proxyPass = "http://127.0.0.1:29919";
-    };
-    locations."/" = {
-      proxyWebsockets = true;
-      proxyPass = "http://127.0.0.1:29918";
-    };
-  };
+  services.nginx.virtualHosts."reef.geenit.nl".locations =
+    builtins.mapAttrs
+      (
+        n: v:
+        lib.mkMerge [
+          v
+          ({
+            proxyWebsockets = true;
+            extraConfig = ''
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Scheme $scheme;
+              proxy_set_header X-Forwarded-Proto https;
+              proxy_set_header X-Forwarded-Host $host;
+              grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            '';
+          })
+        ]
+      )
+      {
+        "^/(relay|ws-proxy/)" = {
+          proxyPass = "http://127.0.0.1:29919";
+          extraConfig = ''
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "Upgrade";
+            proxy_set_header Host $host;
+            proxy_read_timeout 1d;
+          '';
+        };
+        "^/(signalexchange\.SignalExchange|management\.(ManagementService|ProxyService))/" = {
+          proxyPass = "http://127.0.0.1:29919";
+          extraConfig = ''
+            grpc_pass grpc://netbird_server;
+            grpc_read_timeout 1d;
+            grpc_send_timeout 1d;
+            grpc_socket_keepalive on;
+          '';
+        };
+        "^/(api|oauth2)/" = {
+          proxyPass = "http://127.0.0.1:29919";
+          extraConfig = "proxy_set_header Host $host;";
+        };
+        "/" = {
+          proxyPass = "http://127.0.0.1:29918";
+        };
+      };
 
   modules.nginx.other = [
     {
