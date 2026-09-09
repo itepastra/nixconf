74cbe26af32c92289219fcbaa40c81fc44d90e8a
Noa Aarts <noa@voorwaarts.nl>
2026-09-09 19:15:29 +0200

    fixup! set up atticd

--- modules/atticd/default.nix
+++ modules/atticd/default.nix
@@ -52,7 +52,7 @@
     };
   };
 
-  systemd.services.atticd.serviceConfig.DynamicUser = false;
+  systemd.services.atticd.serviceConfig.DynamicUser = lib.mkForce false;
 
   users.users.atticd = {
     isSystemUser = true;
