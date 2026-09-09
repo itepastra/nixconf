a7ed78bddf2136b6a495d844cbbcd2bb1ff491bd
Noa Aarts <noa@voorwaarts.nl>
2026-09-09 22:55:21 +0200

    Revert "remove priority from trench substituter"
    This reverts commit d82cf2ffdeaadd8d81dfee9b7047b03d5037837c.

--- modules/attic/default.nix
+++ modules/attic/default.nix
@@ -42,7 +42,7 @@ in
   };
 
   nix.settings = {
-    substituters = [ "http://trench.reef/anemone" ];
+    substituters = [ "http://trench.reef/anemone?priority=10" ];
     trusted-substituters = [ "http://trench.reef/anemone" ];
   };
 }
