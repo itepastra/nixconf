62acfc727e62de6da0a1310de5016fa9684252d6
Noa Aarts <noa@voorwaarts.nl>
2026-09-09 11:28:03 +0200

    meshchatx: fix typo in description

--- packages/meshchatx/default.nix
+++ packages/meshchatx/default.nix
@@ -23,7 +23,7 @@ appimageTools.wrapType2 rec {
   ];
 
   meta = {
-    description = "Comminucation client for reticulum";
+    description = "Communication client for reticulum";
     homepage = "https://meshchatx.com/";
     downloadPage = "https://meshchatx.com/download";
     license = lib.licenses.bsd0;
