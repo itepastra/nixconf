ca8c228c997b8319d58faf568803ce5e2ee4c379
Noa Aarts <noa@voorwaarts.nl>
2026-09-10 17:20:42 +0200

    remove attic watch store service as it is buggy

--- modules/attic/default.nix
+++ modules/attic/default.nix
@@ -29,18 +29,6 @@ in
     }"
   ];
 
-  systemd.services.attic-watch-store = {
-    description = "Attic watch store";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${attic}/bin/attic watch-store --ignore-upstream-cache-filter trench:anemone";
-      Restart = "on-failure";
-      RestartSec = 1;
-    };
-  };
-
   nix.settings = {
     substituters = [ "http://trench.reef/anemone?priority=10" ];
     trusted-public-keys = [ "anemone:f/wBQ8yB5geTn96NjwRfbcoEvr8QuykN0iu0Rf2zUC8=" ];
