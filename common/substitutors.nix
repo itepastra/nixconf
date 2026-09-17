62bcc04825b74cd91a898d5b785010e9d987f859
Noa Aarts <noa@voorwaarts.nl>
2026-09-17 12:15:37 +0200

    remove broken binary cache

--- common/substitutors.nix
+++ common/substitutors.nix
@@ -13,14 +13,12 @@
       substituters = [
         "https://cosmic.cachix.org/"
         "https://cache.iog.io"
-        "https://cuda-maintainers.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
